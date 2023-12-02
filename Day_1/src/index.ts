c5f982912b8078ed591c3d054ad4589db12f75f6
fangg19 <alexjohnny6@gmail.com>
2023-12-02 14:30:24 +0200

    aoc-day_1

---
+++ Day_1/src/index.ts
@@ -0,0 +1,41 @@
+import fs from "fs";
+
+function getLines() {
+  try {
+    const data = fs.readFileSync("./input.txt", "utf-8");
+    return data.split("\n");
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function main() {
+  const lines = getLines();
+  let sum = 0;
+  let arrOfStringNumbers: string[] = [];
+
+  if (Array.isArray(lines)) {
+    for (const line of lines) {
+      let stringNum = "";
+
+      for (const char of line) {
+        if (!isNaN(parseInt(char))) {
+          stringNum += char;
+        }
+      }
+
+      if (!isNaN(parseInt(stringNum))) {
+        arrOfStringNumbers.push(stringNum);
+      }
+    }
+  }
+
+  for (const stringNum of arrOfStringNumbers) {
+    sum += parseInt(stringNum);
+  }
+
+  return sum;
+}
+
+console.log("The sum of all numbers in the file is: ", main());
+main();
