e1faa9f2dc35d47487fd7f2e5d5b89a4f73010f4
fangg19 <alexjohnny6@gmail.com>
2023-12-03 21:26:50 +0200

    day_1 part_two

--- Day_1/index.ts
+++ Day_1/index.ts
@@ -1,41 +1,116 @@
-import fs from "fs";
+import * as fs from "fs";
 
-function getLines() {
+const stringNumbers = [
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+];
+
+function getRawLines() {
   try {
     const data = fs.readFileSync("./input.txt", "utf-8");
-    return data.split("\n");
+    return data.split("\n").map((line) => line.replace("\r", "")) ?? [];
   } catch (err) {
     console.error(err);
+    return [];
   }
+}
+
+function getFirstNumber(lines: string[], length: number): string[] {
+  let newLine: string;
+  if (length === 0) {
+    return lines;
+  }
+  const newArr = lines.map((line) => {
+    if (
+      !isNaN(parseInt(line.charAt(0))) &&
+      !isNaN(parseInt(line.charAt(line.length - 1)))
+    ) {
+      return line;
+    }
+
+    if (!isNaN(parseInt(line.charAt(0)))) {
+      return line;
+    }
+
+    for (const stringNum of stringNumbers) {
+      const numberToReplace = (stringNumbers.indexOf(stringNum) + 1).toString();
+
+      if (line.startsWith(stringNum)) {
+        newLine = line.replace(stringNum, numberToReplace);
+        break;
+      }
+
+      newLine = line.slice(1);
+    }
+
+    return newLine;
+  });
+  return getFirstNumber(newArr, length - 1);
+}
+
+function getLastNumber(lines: string[], length: number): string[] {
+  let newLine: string;
+  if (length === 0) {
+    return lines;
+  }
+  const newArr = lines.map((line) => {
+    if (
+      !isNaN(parseInt(line.charAt(0))) &&
+      !isNaN(parseInt(line.charAt(line.length - 1)))
+    ) {
+      return line;
+    }
+
+    if (!isNaN(parseInt(line.charAt(line.length - 1)))) {
+      return line;
+    }
+
+    for (const stringNum of stringNumbers) {
+      const numberToReplace = (stringNumbers.indexOf(stringNum) + 1).toString();
+
+      if (line.endsWith(stringNum)) {
+        newLine = line.replace(stringNum, numberToReplace);
+        break;
+      }
+
+      newLine = line.substring(0, line.length - 1);
+    }
+
+    return newLine;
+  });
+
+  return getLastNumber(newArr, length - 1);
 }
 
 function main() {
-  const lines = getLines();
-  let sum = 0;
-  let arrOfStringNumbers: string[] = [];
+  const rawLines = getRawLines();
+  let arrOfStringNumbers: number[] = [];
 
-  if (Array.isArray(lines)) {
-    for (const line of lines) {
-      let stringNum = "";
+  const firstWordNumber = getFirstNumber(rawLines, rawLines.length);
+  const lines = getLastNumber(firstWordNumber, rawLines.length);
 
-      for (const char of line) {
-        if (!isNaN(parseInt(char))) {
-          stringNum += char;
-        }
-      }
-
-      if (stringNum !== "") {
-        const num = stringNum[0] + stringNum[stringNum.length - 1];
-        arrOfStringNumbers.push(num);
+  for (const line of lines) {
+    let stringNum = "";
+    for (const char of line) {
+      if (!isNaN(parseInt(char))) {
+        stringNum += char;
       }
     }
-  }
-  for (const stringNum of arrOfStringNumbers) {
-    sum += parseInt(stringNum);
+
+    if (stringNum !== "") {
+      const number = Number(stringNum[0] + stringNum[stringNum.length - 1]);
+      arrOfStringNumbers.push(number);
+    }
   }
 
-  return sum;
+  arrOfStringNumbers.reduce((a, b) => a + b);
 }
 
-console.log("The sum of all numbers in the file is: ", main());
 main();
